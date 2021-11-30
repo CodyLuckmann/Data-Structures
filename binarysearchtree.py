3102c610e82996bb37e33cb33d7557629230b16a
CodyLuckmann <cluckmann@live.com>
2021-11-30 11:40:03 -0600

    created method for inserting a node into the tree

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -5,21 +5,30 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
             
-    def insert_node (self, newValue):
-        node = Node(newValue)
+    def insert_node (self, new_value):
+        node = Node(new_value)
 
         # if tree is empty make new node, declare it as root
         if self.root is None:
             self.root = node
-        else:
-            temp_node = self.root
-            # if tree not empty, insert node into tree
-            # if new value is less than value of root, add to left subtree
-            if newValue < temp_node:
-                self.root.leftChild = node
+            return
+        current_node = self.root
+        prev_node = None
+           
+        while current_node:
+            prev_node = current_node
+            if new_value < current_node.data:
+                current_node = current_node.leftChild
             else:
-            # if new value greater than root, add to right subtree
-                self.root.rightChild = node
+                current_node = current_node.rightChild
+            
+        if new_value < prev_node.data:
+            prev_node.leftChild = node
+        else:
+            prev_node.rightChild = node
+            
+            
+           
             
             
 
