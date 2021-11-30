ac4337bdab4d945086516517fc0769c616b06177
CodyLuckmann <cluckmann@live.com>
2021-11-30 10:40:53 -0600

    added child to be clearer

--- node.py
+++ node.py
@@ -3,7 +3,8 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.left = None
-        self.right = None
+        self.rightChild = None
+        self.leftChild = None
+        
         
     
