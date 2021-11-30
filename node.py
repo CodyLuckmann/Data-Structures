6abe1fba1429b55303a0762e6d864489b5c53ae7
CodyLuckmann <cluckmann@live.com>
2021-11-30 12:09:56 -0600

    Changed child variable to read easier

--- node.py
+++ node.py
@@ -3,8 +3,8 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.rightChild = None
-        self.leftChild = None
+        self.right_child = None
+        self.left_child = None
         
         
     
